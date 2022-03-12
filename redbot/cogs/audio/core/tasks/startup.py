cd3af3236364d0ef71f3e8ce0fd5597c7c2e122e
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-03-12 19:28:58 +0000

    Fix Unhandled error - https://github.com/Cog-Creators/Red-DiscordBot/pull/5593#issuecomment-1065942979

--- redbot/cogs/audio/core/tasks/startup.py
+++ redbot/cogs/audio/core/tasks/startup.py
@@ -134,7 +134,7 @@ class StartUpTasks(MixinMeta, metaclass=CompositeMetaClass):
                             player = await lavalink.connect(vc, deafen=auto_deafen)
                             player.store("notify_channel", notify_channel_id)
                             break
-                        except IndexError:
+                        except NodeNotFound:
                             await asyncio.sleep(5)
                             tries += 1
                         except Exception as exc:
@@ -201,7 +201,7 @@ class StartUpTasks(MixinMeta, metaclass=CompositeMetaClass):
                         player = await lavalink.connect(vc, deafen=auto_deafen)
                         player.store("notify_channel", notify_channel_id)
                         break
-                    except IndexError:
+                    except NodeNotFound:
                         await asyncio.sleep(5)
                         tries += 1
                     except Exception as exc:
