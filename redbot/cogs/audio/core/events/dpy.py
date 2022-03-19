600e039c950bc6b139ad80bfdbfc4654641eaa7a
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-03-19 22:18:41 +0000

    fix translation in audio/core/events/dpy.py

--- redbot/cogs/audio/core/events/dpy.py
+++ redbot/cogs/audio/core/events/dpy.py
@@ -23,11 +23,136 @@ from redbot.core.utils.chat_formatting import box, humanize_list, underline, bol
 
 from ...errors import TrackEnqueueError, AudioError
 from ..abc import MixinMeta
-from ..cog_utils import HUMANIZED_PERM, CompositeMetaClass, DANGEROUS_COMMANDS
+from ..cog_utils import CompositeMetaClass
 
 log = getLogger("red.cogs.Audio.cog.Events.dpy")
-_ = Translator("Audio", Path(__file__))
+_T = Translator("Audio", Path(__file__))
+_ = lambda s: s
 RE_CONVERSION: Final[Pattern] = re.compile('Converting to "(.*)" failed for parameter "(.*)".')
+HUMANIZED_PERM = {
+    "create_instant_invite": _("Create Instant Invite"),
+    "kick_members": _("Kick Members"),
+    "ban_members": _("Ban Members"),
+    "administrator": _("Administrator"),
+    "manage_channels": _("Manage Channels"),
+    "manage_guild": _("Manage Server"),
+    "add_reactions": _("Add Reactions"),
+    "view_audit_log": _("View Audit Log"),
+    "priority_speaker": _("Priority Speaker"),
+    "stream": _("Go Live"),
+    "read_messages": _("Read Text Channels & See Voice Channels"),
+    "send_messages": _("Send Messages"),
+    "send_tts_messages": _("Send TTS Messages"),
+    "manage_messages": _("Manage Messages"),
+    "embed_links": _("Embed Links"),
+    "attach_files": _("Attach Files"),
+    "read_message_history": _("Read Message History"),
+    "mention_everyone": _("Mention @everyone, @here, and All Roles"),
+    "external_emojis": _("Use External Emojis"),
+    "view_guild_insights": _("View Server Insights"),
+    "connect": _("Connect"),
+    "speak": _("Speak"),
+    "mute_members": _("Mute Members"),
+    "deafen_members": _("Deafen Members"),
+    "move_members": _("Move Members"),
+    "use_voice_activation": _("Use Voice Activity"),
+    "change_nickname": _("Change Nickname"),
+    "manage_nicknames": _("Manage Nicknames"),
+    "manage_roles": _("Manage Roles"),
+    "manage_webhooks": _("Manage Webhooks"),
+    "manage_emojis": _("Manage Emojis"),
+}
+
+DANGEROUS_COMMANDS = {
+    "command_llsetup_java": _(
+        "This command will change the executable path of Java, "
+        "this is useful if you have multiple installations of Java and the default one is causing issues. "
+        "Please don't change this unless you are certain that the Java version you are specifying is supported by Red. "
+        "The default and supported version is currently Java 11."
+    ),
+    "command_llsetup_heapsize": _(
+        "This command will change the maximum RAM allocation for the managed Lavalink node, "
+        "usually you will never have to change this, "
+        "before considering changing it please consult our support team."
+    ),
+    "command_llsetup_external": _(
+        "This command will disable the managed Lavalink node, "
+        "if you toggle this command you must specify an external Lavalink node to connect to, "
+        "if you do not do so Audio will stop working."
+    ),
+    "command_llsetup_host": _(
+        "This command is used to specify the IP which will be used by Red to connect to an external Lavalink node. "
+    ),
+    "command_llsetup_password": _(
+        "This command is used to specify the authentication password used by Red to connect to an "
+        "external Lavalink node."
+    ),
+    "command_llsetup_secured": _(
+        "This command is used toggle between secured and unsecured connections to an external Lavalink node."
+    ),
+    "command_llsetup_wsport": _(
+        "This command is used to specify the connection port used by Red to connect to an external Lavalink node."
+    ),
+    "command_llsetup_config_host": _(
+        "This command specifies which network interface and IP the managed Lavalink node will bind to, "
+        "by default this is 'localhost', "  # meaning it will bind to all interfaces
+        "only change this if you want the managed Lavalink node to bind to a specific IP/interface."
+    ),
+    "command_llsetup_config_token": _(
+        "This command changes the authentication password required to connect to this managed node."
+        "The default value is 'youshallnotpass'."
+    ),
+    "command_llsetup_config_port": _(
+        "This command changes the connection port used to connect to this managed node, "
+        "only change this if the default port '2333' is causing conflicts with existing applications."
+    ),
+    "command_llsetup_config_source_http": _(
+        "This command toggles the support of direct url streams like Icecast or Shoutcast streams. "
+        "An example is <http://ice6.somafm.com/gsclassic-128-mp3>; "
+        "Disabling this will make the bot unable to play any direct url steam content."
+    ),
+    "command_llsetup_config_source_bandcamp": _(
+        "This command toggles the support of Bandcamp audio playback. "
+        "An example is <http://deaddiskdrive.bandcamp.com/track/crystal-glass>; "
+        "Disabling this will make the bot unable to play any Bandcamp content",
+    ),
+    "command_llsetup_config_source_local": _(
+        "This command toggles the support of local track audio playback. "
+        "for example `/mnt/data/my_super_funky_track.mp3`; "
+        "Disabling this will make the bot unable to play any local track content."
+    ),
+    "command_llsetup_config_source_soundcloud": _(
+        "This command toggles the support of Soundcloud playback. "
+        "An example is <https://soundcloud.com/user-103858850/tilla>; "
+        "Disabling this will make the bot unable to play any Soundcloud content."
+    ),
+    "command_llsetup_config_source_youtube": _(
+        "This command toggles the support of YouTube playback (Spotify depends on YouTube). "
+        "Disabling this will make the bot unable to play any YouTube content, "
+        "this includes Spotify."
+    ),
+    "command_llsetup_config_source_twitch": _(
+        "This command toggles the support of Twitch playback. "
+        "An example of this is <https://twitch.tv/monstercat>; "
+        "Disabling this will make the bot unable to play any Twitch content."
+    ),
+    "command_llsetup_config_source_vimeo": _(
+        "This command toggles the support of Vimeo playback. "
+        "An example of this is <https://vimeo.com/157743578>; "
+        "Disabling this will make the bot unable to play any Vimeo content."
+    ),
+    "command_llsetup_config_server_framebuffer": _(
+        "This setting controls the managed nodes framebuffer, "
+        "Do not change this unless instructed."
+    ),
+    "command_llsetup_config_server_buffer": _(
+        "This setting controls the managed nodes NAS buffer, "
+        "Do not change this unless instructed."
+    ),
+    "command_llsetup_reset": _("This command will reset every setting changed by `[p]llset`."),
+}
+
+_ = _T
 
 
 class DpyEvents(MixinMeta, metaclass=CompositeMetaClass):
@@ -79,7 +204,7 @@ class DpyEvents(MixinMeta, metaclass=CompositeMetaClass):
                 for perm, value in missing_perms.items():
                     text += "{perm}: [{status}]\n".format(
                         status=_("Enabled") if value else _("Disabled"),
-                        perm=HUMANIZED_PERM.get(perm),
+                        perm=_(HUMANIZED_PERM.get(perm, perm)),
                     )
                 text = text.strip()
                 if current_perms.send_messages and current_perms.read_messages:
@@ -124,7 +249,7 @@ class DpyEvents(MixinMeta, metaclass=CompositeMetaClass):
                     "If you wish to continue, enter this case sensitive token without spaces as your next message."
                     "\n\n{confirm_token}"
                 ).format(
-                    template=DANGEROUS_COMMANDS[ctx.command.callback.__name__],
+                    template=_(DANGEROUS_COMMANDS[ctx.command.callback.__name__]),
                     confirm_token=box(confirm_token, lang="py"),
                 )
                 sent = await ctx.send(message)
